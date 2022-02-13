76a1853913b212fd59e8cccbfb481611aff2f8c1
antoniorv6@gmail.com <ariosvila@gmail.com>
2022-02-13 13:56:52 +0100

    Included shell scripts

---
+++ run_span_tf.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python train_span_tf.py --data_path Data/Synth6Staves/ --model_name SPANTF --corpus_name ToyPrimus --batch_size 1
---
+++ run_span_torch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python train_span_torch.py --data_path Data/Synth6Staves/ --model_name SPANTF --corpus_name ToyPrimus --batch_size 1
