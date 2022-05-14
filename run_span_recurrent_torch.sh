73fd5abe3460b437a07724cc1db2e6b3f577acf7
antoniorv6 <ariosvila@gmail.com>
2022-05-14 11:58:01 +0200

    Included sseq decoding

--- run_span_recurrent_torch.sh
+++ run_span_recurrent_torch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python train_recurrent_span.py --data_path Data/SEILS --model_name SPAN_AUG --corpus_name SEILS --batch_size 1 --patience 10
+python train_recurrent_span.py --data_path Data/SEILS --model_name SPAN_AUG --corpus_name SEILS --encoding sseq --batch_size 1 --patience 10
